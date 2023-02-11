158cdad82b0a4c7ef33ce83f284bc1cf24488309
Scott Wu <58385792+coffeemilktea@users.noreply.github.com>
2023-02-10 21:42:14 -0800

    Update psport.ps1

--- psport.ps1
+++ psport.ps1
@@ -1,10 +1,10 @@
 $ports = Read-Host "Enter the port numbers you want to open:"
 $direction = Read-Host "Enter whether Inbound or Outbound rule:"
 
-# Split the input into an array of port numbers
+# Set Array for multiple ports
 $portArray = $ports.Split(',')
 
-# Loop through the array and create a firewall rule for each port
+# Use inputs to generate firewall rules
 foreach ($port in $portArray) {
   New-NetFirewallRule -DisplayName "Allow $direction Traffic on Port $port" -Direction $direction -Action Allow -Protocol TCP -LocalPort $port -Profile Any
 }
