5d41a9844275403d2340f8fdc2b62ff4fbbab660
Scott Wu <58385792+coffeemilktea@users.noreply.github.com>
2023-02-15 03:02:21 -0800

    Update psport.ps1

--- psport.ps1
+++ psport.ps1
@@ -1,14 +1,33 @@
+#Script made to open firewall ports
+#Testing
+
 $ports = Read-Host "Enter the port numbers you want to open:"
 # direction must be Inbound or Outbound
 $direction = Read-Host "Enter whether Inbound or Outbound:"
 
+# Validate user input for direction
+if ($direction -ne "Inbound" -and $direction -ne "Outbound") {
+  Write-Host "Invalid direction. Please enter either 'Inbound' or 'Outbound'."
+  Exit
+}
 
 # Set Array for multiple ports
 $portArray = $ports.Split(',')
 
 # Use inputs to generate firewall rules
-foreach ($port in $portArray) {
-  New-NetFirewallRule -DisplayName "Allow $direction Traffic on Port $port" -Direction $direction -Action Allow -Protocol TCP -LocalPort $port -Profile Any
+$errors = foreach ($port in $portArray) {
+  try {
+    New-NetFirewallRule -DisplayName "Allow $direction Traffic on Port $port" -Direction $direction -Action Allow -Protocol TCP -LocalPort $port -Profile Any -ErrorAction Stop
+  }
+  catch {
+    $_.Exception.Message
+  }
 }
 
-Write-Host "Firewall rules created to allow $direction traffic on ports $ports"
+# Check for errors and output message
+if ($errors) {
+  Write-Host "The following errors occurred while creating firewall rules:`n$errors" -ForegroundColor Red
+}
+else {
+  Write-Host "Firewall rules created to allow $direction traffic on ports $ports"
+}
